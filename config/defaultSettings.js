c435953afdf9ab081eb740da57fadc30264989bb
myan1aha <826566157@qq.com>
2020-06-12 15:18:57 +0800

    first commit 20200612

---
+++ config/defaultSettings.js
@@ -0,0 +1,16 @@
+export default {
+  navTheme: 'white',
+  primaryColor: '#365499',
+  layout: 'sidemenu',//sidemenu
+  contentWidth: 'Fluid',
+  fixedHeader: true,
+  siderWidth: 230,
+  autoHideHeader: false,
+  fixSiderbar: false,
+  colorWeak: false,
+  menu: {
+    locale: true,
+  },
+  title: 'prjname',
+  iconfontUrl: '',//图标库 在header上引用时需要加上icon-前缀
+};
