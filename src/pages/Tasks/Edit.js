e9564f44e27ab60b7b23494e148e9c31b86dc05f
myan1aha <826566157@qq.com>
2020-06-16 18:39:26 +0800

    2020/6/16 commit 3rd

--- src/pages/Tasks/Edit.js
+++ src/pages/Tasks/Edit.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Breadcrumb, Form, Input, Button } from 'antd';
-// eslint-disable-next-line import/no-extraneous-dependencies
-// import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
 import Link from 'umi/link';
 import options from './options'
-// import CreateForm from './createForm'
+import MultiInput from './MultiInput'
+// import Multi from './MuInput';
 
 const { TextArea } = Input;
 const layout = {
@@ -21,20 +20,38 @@ const tailLayout = {
         span: 8,
     },
 };
-// const formItemLayout = {
-//     labelCol: {
-//         xs: { span: 24 },
-//         sm: { span: 4 },
-//     },
-//     wrapperCol: {
-//         xs: { span: 24 },
-//         sm: { span: 20 },
-//     },
-// };
+const formItemLayout = {
+    labelCol: {
+        xs: { span: 4 },
+        sm: { span: 4 },
+    },
+    wrapperCol: {
+        xs: { span: 8 },
+        sm: { span: 8 },
+    },
+};
 
 // eslint-disable-next-line react/prefer-stateless-function
 class TasksEdit extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            // eslint-disable-next-line react/no-unused-state
+            form: {},
+        }
+    }
+
+    onAdd = () => {
+        console.log('add');
+        const formInfo = document.querySelector('.form_info');
+        this.setState({
+            // eslint-disable-next-line react/no-unused-state
+            form: formInfo,
+        })
+    }
+
     render() {
+        // const { getFieldDecorator } = this.props.form;
         return (
             <div>
                 <Breadcrumb >
@@ -52,6 +69,7 @@ class TasksEdit extends React.Component {
                 <Form
                     {...layout}
                     name="basic"
+                    className="form_info"
                 >
                     {
                         options.map(item => {
@@ -75,11 +93,17 @@ class TasksEdit extends React.Component {
                                         </Form.Item>
                                     );
                                 case 'insertForm':
+                                    // console.log(item.label);
                                     return (
-                                        <Form.Item
-                                        
+                                        <Form.Item label={item.label} {...formItemLayout} key={item.key}
+                                        rules={[
+                                            {
+                                                required: true,
+                                                message: `请输入${item.label}`,
+                                            },
+                                            ]}
                                         >
-                                            <Input placeholder="222"></Input>
+                                           <MultiInput label={item.label} required={!item.noneed} />
                                         </Form.Item>
                                     );
                                 default: return null;
@@ -87,11 +111,12 @@ class TasksEdit extends React.Component {
                         })
                     }
                     <Form.Item {...tailLayout}>
-                        <Button type="primary">保存</Button>
+                        <Button type="primary" onClick={this.onAdd}>保存</Button>
                         <Button type="dashed" style={{ marginLeft: '10px' }}>取消</Button>
                     </Form.Item>
                 </Form>
             </div>
+
         )
     }
 }
--- src/pages/Tasks/options.js
+++ src/pages/Tasks/options.js
@@ -1,8 +1,8 @@
 const options = [
     { key: 1, name: 'proName', label: '项目名称', type0: 'text', type: 'input' },
     { key: 2, name: 'taskName', label: '任务名称', type0: 'text', type: 'input' },
-    { key: 3, name: 'originForm', label: '来源表', type0: 'text', type: 'input' },
-    { key: 4, name: 'targetForm', label: '目标表', type0: 'text', type: 'input' },
+    { key: 3, name: 'originForm', label: '来源表', type0: 'insertForm', type: 'input' },
+    { key: 4, name: 'targetForm', label: '目标表', type0: 'insertForm', type: 'input' },
     { key: 5, name: 'taskDependency', label: '解析任务依赖', type0: 'text', type: 'input' },
     { key: 6, name: 'upNode', label: '上游节点', type0: 'text', type: 'input' },
     { key: 7, name: 'order', label: '命令', type0: 'text', type: 'input' },
